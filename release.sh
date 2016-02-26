dd977408aae4429402ad9c9975f31366d73107e0
Nathan Bleigh <nathan.bleigh@gmail.com>
2016-02-25 19:25:45 -0600

    Only release gzip

--- release.sh
+++ release.sh
@@ -2,4 +2,4 @@
 
 tar -zcvf dist/aws-rollout-linux-x86_64.tar.gz dist/aws-rollout
 latest_tag=$(git describe --tags `git rev-list --tags --max-count=1`)
-github-release "oberd/aws-rollout" "$latest_tag" "$(git rev-parse --abbrev-ref HEAD)" "" 'dist/*'
+github-release "oberd/aws-rollout" "$latest_tag" "$(git rev-parse --abbrev-ref HEAD)" "" 'dist/*.tar.gz'
